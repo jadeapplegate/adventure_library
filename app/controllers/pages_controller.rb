5b2d22156ee7b023e21c39586c6914cfaf6231da
Jade <jade.applegate@gmail.com>
2014-05-16 16:50:35 -0700

    user can now create an adventure with a start, story pages, and end. Saves to db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,20 @@ before_action :load_adventure
     @page = @adventure.pages.new
   end
 
-  # def create
-  #   @page = @adventure.page.new(page_params)
-  #   if @page.save
-  #     @page.name = 'new'
-  #     redirect_to [@adventure, @page]
-  #   else
-  #     flash[:errors] = @page.errors.full_messages
-  #     render :edit
-  #   end
-  # end
+  def create
+    @page = @adventure.pages.new(page_params)
+    @page.adventure_id = @adventure.id
+    if @page.save
+      if@page.name == 'end'
+        redirect_to root_path
+      else
+        redirect_to new_adventure_page_path(@adventure)
+      end
+    else
+      flash[:errors] = @page.errors.full_messages
+      render :new
+    end
+  end
 
   def show
     # @page = Page.find(params[:id])
@@ -28,7 +32,7 @@ before_action :load_adventure
   end
 
   private
-  def pages_params
+  def page_params
     params.require(:page).permit(:name, :text)
   end
 
