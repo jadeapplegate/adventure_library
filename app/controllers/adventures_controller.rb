f04cf9ba84cf2717100b5cdbd1212a196d6c58ec
Jade <jade.applegate@gmail.com>
2014-05-15 21:32:32 -0700

    added restful routes to adventures controller

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,52 @@
 class AdventuresController < ApplicationController
+
+  def index
+    @adventures = Adventure.all
+  end
+
+  def show
+    @adventure = params[:id]
+    @start = Adventure.find(@adventure).pages.find_by(name: "start").id
+    redirect_to adventure_page_path(@adventure, @start)
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+    @adventure = Adventure.new(adventure_params)
+    @adventure.GUID = SecureRandom.urlsafe_base64(10)
+    if @adventure.save
+      redirect_to adventures_path
+    else
+      flash[:errors] = @adventure.errors.full_messages
+      render :edit
+    end
+  end
+
+  def edit
+    @adventure = Adventure.find(params[:id])
+  end
+
+  def update
+    @adventure = Adventure.find(params[:id])
+    if @adventure.update(adventure_params)
+      redirect_to adventure_path(@adventure)
+    else
+      flash[:errors] = @adventure.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @adventure.destroy
+    redirect_to root_path
+  end
+
+  private
+    def adventure_params
+      params.require(:adventure).permit(:title, :author) 
+    end
+
 end
