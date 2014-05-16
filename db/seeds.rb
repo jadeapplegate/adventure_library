f004aa175c2c77ad00c26d403cb665bf0ff6990b
Jade <jade.applegate@gmail.com>
2014-05-15 21:33:43 -0700

    added second adventure to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 name = "Jade"
-adv = Adventure.create!(:title => "#{name}'s Test Adventure",
+adv1 = Adventure.create!(:title => "#{name}'s Test Adventure",
 	:author => name)
 adv.pages.create!(:name => "start",
 	:text => "This is a great text adventure which is beginning right now!  I can't wait to [[see the end|end]]")
 adv.pages.create!(:name => "end",
 	:text => "Wow that adventure was amazing!")
+
+adv2 = Adventure.create!(:title => "#{name}'s Second Test Adventure",
+  :author => name)
+adv.pages.create!(:name => "start",
+  :text => "This is the second test adventure!  I can't wait to [[see the end|end]]")
+adv.pages.create!(:name => "end",
+  :text => "Wow that adventure was amazing!")
